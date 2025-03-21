df4a98181fb28925e597323a7fbdd8c89e3316fa
halionaz <samhsj@korea.ac.kr>
2025-03-21 17:00:07 +0900

    init: Repository Setting & Init Amplitude

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import useAmplitude from './hooks/useAmplitude';
+
+function App() {
+  useAmplitude();
+
+  return <>까치는 비가 와도 운다</>;
+}
+
+export default App;
---
+++ src/hooks/useAmplitude.tsx
@@ -0,0 +1,16 @@
+import { useEffectOnce } from './useEffectOnce';
+
+import * as amplitude from '@amplitude/analytics-browser';
+
+const useAmplitude = () => {
+  useEffectOnce(() => {
+    amplitude.init(import.meta.env.VITE_API_AMPLITUDE_API_KEY, {
+      autocapture: true,
+    });
+    console.log(import.meta.env.VITE_API_AMPLITUDE_API_KEY);
+    // 이런 식으로 체크
+    // amplitude.track('Sign Up');
+  });
+};
+
+export default useAmplitude;
