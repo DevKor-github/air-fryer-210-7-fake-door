96a098620652ea7b07378636cf82f8c5b182c8d8
halionaz <samhsj@korea.ac.kr>
2025-03-23 00:08:57 +0900

    fix: delete amplitude un-unique userID

--- src/providers/AmplitudeProvider.tsx
+++ src/providers/AmplitudeProvider.tsx
@@ -4,16 +4,12 @@ import { useEffectOnce } from '@/hooks/useEffectOnce';
 import * as amplitude from '@amplitude/analytics-browser';
 
 const AmplitudeProvider = ({ children }: PropsWithChildren) => {
-  const searchParams = new URLSearchParams(document.location.search);
-  const from = searchParams.get('c');
+  // const searchParams = new URLSearchParams(document.location.search);
+  // const from = searchParams.get('c');
   useEffectOnce(() => {
-    amplitude.init(
-      import.meta.env.VITE_API_AMPLITUDE_API_KEY,
-      from ?? undefined,
-      {
-        autocapture: true,
-      }
-    );
+    amplitude.init(import.meta.env.VITE_API_AMPLITUDE_API_KEY, {
+      autocapture: true,
+    });
   });
 
   return <>{children}</>;
